00cfa75e4e0d187e97d028f84fa4815c6ae418e0
dfoulhoux <dfoulhoux@openstudio.fr>
2026-07-01 14:30:42 +0200

    base for asset mapper instead of webpack encore

--- importmap.php
+++ importmap.php
@@ -12,15 +12,15 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'app' => [
-        'path' => 'backoffice/app.js',
+    'backOfficeApp' => [
+        'path' => './assets/app.js',
         'entrypoint' => true,
     ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
     '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+        'path' => '../../../vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
     'bootstrap' => [
         'version' => '5.3.8',
